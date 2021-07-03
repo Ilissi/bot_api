e6bf3640fb66c66607ad17cd1318e1fb89ee6ae9
Виктор Кузьминский <maksimboginic@Maksims-MacBook-Pro.local>
2021-07-03 23:26:52 +0300

    secret key

--- controller/strategy.Api.Controller.js
+++ controller/strategy.Api.Controller.js
@@ -1,11 +1,16 @@
+let path = require('path')
+
 const strategyController = require('./strategy.Controller')
 
-require('dotenv').config({ path: '/Users/kuzminskyi/WebstormProjects/bot_api/.env' })
+require('dotenv').config({ path: path.join(__dirname, '.env')})
 
 exports.ideas_list = [ async (req, res) =>{
     if (req.body.secret_key===process.env.SECRET_KEY){
-        let ideas = await strategyController.selectStrategy(req.body.from)
-        res.send(ideas.rows)
+        let ideas = await strategyController.selectStrategy(req.body.from);
+        res.send(ideas.rows);
+    }
+    else if (req.body.secret_key!==process.env.SECRET_KEY){
+        res.status(500).send('Wrong secret key!');
     }
     else {
         res.status(500).send('Something failed!');
