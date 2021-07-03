911f1c8b971c62da9cb869e73f48d59a3987aedd
Виктор Кузьминский <maksimboginic@Maksims-MacBook-Pro.local>
2021-07-04 00:34:22 +0300

    poort

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ httpsOptions = {
  * Get port from environment and store in Express.
  */
 
-let port = normalizePort(process.env.PORT || '3000');
+let port = normalizePort(process.env.PORT || '3005');
 app.set('port', port);
 
 /**
